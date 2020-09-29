43bd711fd13ead18360e6ae8e03e91378f58d948
DeniiseB <denisebradley92@hotmail.com>
2020-09-29 10:58:15 +0200

    Looping through real floor numbers

--- src/com/company/SkyScraper.java
+++ src/com/company/SkyScraper.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class SkyScraper {
+
+    static public void floorList(int totalRealFloors){
+        for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
+            System.out.println("realFloorNo: " + realFloor);
+        }
+    }
+
+
 }
