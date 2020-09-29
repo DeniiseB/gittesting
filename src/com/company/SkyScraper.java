8114fb4497e8c363c4989980b9337039901382af
DeniiseB <denisebradley92@hotmail.com>
2020-09-29 11:17:32 +0200

    The list shows corresponding fake floor numbers

--- src/com/company/SkyScraper.java
+++ src/com/company/SkyScraper.java
@@ -3,8 +3,13 @@ package com.company;
 public class SkyScraper {
 
     static public void floorList(int totalRealFloors){
+
+        var fakeFloor = 1;
         for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
-            System.out.println("realFloorNo: " + realFloor);
+            System.out.println("realFloorNo: " + realFloor + " fakeFloor " + fakeFloor);
+            do{
+            fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
         }
     }
 
